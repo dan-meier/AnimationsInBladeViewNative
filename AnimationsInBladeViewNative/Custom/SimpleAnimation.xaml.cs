43d9a5310804fdc8430c3c226afe0f08b56b2622
Dan Meier <Dan.Meier@FactorySoftworks.com>
2020-12-14 11:12:26 -0800

    Modified to use FadeInThemeAnimation and FadeOutThemeAnimation rather than changing the opacity in the animation.

--- AnimationsInBladeViewNative/Custom/SimpleAnimation.xaml.cs
+++ AnimationsInBladeViewNative/Custom/SimpleAnimation.xaml.cs
@@ -12,24 +12,26 @@ namespace AnimationsInBladeViewNative.Custom
             this.InitializeComponent();
         }
 
-
+        private bool isHidden = false;
         private async void AnimateButton_OnTapped(object sender, TappedRoutedEventArgs e)
         {
             AnimateButton.IsEnabled = false;  // Disable the button while the animation is in progress
 
 
-            ElementTranslationStoryboard.Children[0].SetValue( DoubleAnimation.FromProperty, Element.Opacity == 0 ? -200 : 0 );
-            ElementTranslationStoryboard.Children[0].SetValue( DoubleAnimation.ToProperty,   Element.Opacity == 0 ? 0 : -200 );
-
-            ElementTranslationStoryboard.Children[1].SetValue( DoubleAnimation.FromProperty, Element.Opacity );
-            ElementTranslationStoryboard.Children[1].SetValue( DoubleAnimation.ToProperty,   Element.Opacity == 0 ? 1 : 0 );
-
+            ElementTranslationStoryboard.Children[0].SetValue( DoubleAnimation.FromProperty, isHidden ? -200 : 0 );
+            ElementTranslationStoryboard.Children[0].SetValue( DoubleAnimation.ToProperty,   isHidden ? 0 : -200 );
             ElementTranslationStoryboard.Begin();
+
+            if (isHidden) FadeInStoryboard.Begin();
+            else          FadeOutStoryboard.Begin();
 
 
             await Task.Delay( 1000 );
-            AnimateButton.Content = (string)AnimateButton.Content == "Fly In" ? "Fly Out" : "Fly In";
             AnimateButton.IsEnabled = true; // Re-enable the button when the animation is complete
+
+            
+            AnimateButton.Content = (string)AnimateButton.Content == "Fly In" ? "Fly Out" : "Fly In";
+            isHidden              = ! isHidden;
         }
     }
 }
